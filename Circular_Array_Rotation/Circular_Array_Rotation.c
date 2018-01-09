707f2c7f640a611f6654a98afd166274085f71f4
Zhaozhen Liang <zhaozhen@ualberta.ca>
2018-01-09 15:37:29 -0700

    Update Circular_Array_Rotation.c

--- Circular_Array_Rotation/Circular_Array_Rotation.c
+++ Circular_Array_Rotation/Circular_Array_Rotation.c
@@ -1,22 +1,25 @@
-#!/bin/python
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
 
-import sys
-
-def circularArrayRotation(a, m):
-    # Complete this function
-
-if __name__ == "__main__":
-    n, k, q = raw_input().strip().split(' ')
-    n, k, q = [int(n), int(k), int(q)]
-    a = map(int, raw_input().strip().split(' '))
-    m = []
-    m_i = 0
-    for m_i in xrange(q):
-        m_t = int(raw_input().strip())
-        m.append(m_t)
-    result = circularArrayRotation(a, m)
-    print "\n".join(map(str, result))
-
-
-
+int main(){
+    int n; 
+    int k; 
+    int q; 
+    scanf("%d %d %d",&n,&k,&q);
+    int *a = malloc(sizeof(int) * n);
+    for(int a_i = 0; a_i < n; a_i++){
+       scanf("%d",&a[a_i]);
+    }
+    for(int a0 = 0; a0 < q; a0++){
+        int m; 
+        scanf("%d",&m);
+        printf("%d\n",a[(n-1+(m-(n-1+k%n)%n))%n]);
+    }
+    return 0;
+}
 
